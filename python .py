66daaf2f97a55b424a3e47d454a20301b3aba60f
hari balaji <145361290+haribalji@users.noreply.github.com>
2024-10-08 21:34:53 +0530

    Add files via upload

---
+++ python .py
@@ -0,0 +1,62 @@
+/*
+/* project section view  */
+
+/* * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.containermain{
+  height: 100vh;
+  width:100vw;
+  background-color: #007BFF;
+}
+.herosection{
+    /* margin-left: 400px; */
+    /* left: 50px; */
+    /* margin-top: 60px;
+    top:40px;
+    height: 600px;
+    width: 100%;
+    background-color: #fff;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
+/* } */
+/* .herosectioncontent-photo{
+  
+    /* height: 100%; */
+    /* height: 500px;
+    width:40%;
+    background-color: #f9f9f9;
+    margin-left: 5%;
+    margin-right: 5%;
+    margin-top:50px ; */
+/* } */
+.herosectioncontent-content{
+    height: 500px;
+    width:40%;
+    background-color: #d29d9d;
+    margin-left: 5%;
+    margin-right: 5%;
+    margin-top:50px ; */
+/* }*/
+.image-1{
+    background-image: url(./src/Cooler.jpg);
+    background-size: cover; /* or 'contain' if you want the entire image visible */
+    /* background-position: center;
+    /* background-repeat: no-repeat;
+    height: 100%; */
+    /* width: 100%; */ 
+/* }  */
+
+.text {
+    font-family: 'Verdana', sans-serif; /* Choose your preferred font */
+    font-size: 20px; /* Set the font size */
+    font-weight: bold; /* Optional: Make the text bold */
+    color: #333; /* Text color */
+    text-align: center; /* Center align the text */
+    /* margin: 10px; */
+  } */ 
